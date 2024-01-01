8f0e972772ab9bf865186ef5694e1a2509d68adc
TienNTC <tien.ntc@teko.vn>
2024-01-01 20:38:30 +0700

    config domain

--- lib/src/core/const.dart
+++ lib/src/core/const.dart
@@ -1,4 +1,3 @@
 const agoraAppId = '8c0e34537b24401ebe53e202cea1b299';
-// const domain = 'meeting-app-be.azurewebsites.net';
-const domain = '192.168.1.5:3000';
+const domain = 'meeting-app-be.onrender.com:3000';
 const tokenKey = 'token';
